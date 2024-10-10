a693dc24005dfdd4b911077312051e35e549d1fe
lucas-khaled <khaledbrugger3@gmail.com>
2024-10-09 23:17:28 -0300

    Rename IMainUIManager to IMenuManager

--- Assets/Scripts/UI/IMenuManager.cs
+++ Assets/Scripts/UI/IMenuManager.cs
@@ -4,10 +4,11 @@ using System;
 
 namespace RickAndMemory.UI
 {
-    public interface IMainUIManager
+    public interface IMenuManager
     {
         public void SetLoading(bool active);
         public void Initialize(Action<Layout, IModeManager> callback);
         public void SetEndGame(string text);
+        public void HideAll();
     }
 }
