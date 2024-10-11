1a3bffc05c5d27e0348c57908ae80d28a3a5f80c
lucas-khaled <khaledbrugger3@gmail.com>
2024-10-11 20:52:44 -0300

    Fulfill card ui

--- Assets/Scripts/UI/Card.cs
+++ Assets/Scripts/UI/Card.cs
@@ -1,14 +1,63 @@
 using RickAndMemory.Data;
+using RickAndMemory.Utility;
+using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-namespace RickAndMemory
+namespace RickAndMemory.UI
 {
     public class Card : MonoBehaviour
     {
+        [SerializeField] private GameObject shownObject;
+        [SerializeField] private GameObject hiddenObject;
+        [SerializeField] private Image thumb;
+        [SerializeField] private TMP_Text itemName;
+
+        public Action<CardInfo> onShow;
+
         private CardInfo cardInfo;
+        private bool isSelected;
+
         public void SetInfo(CardInfo info) 
         {
             cardInfo = info;
+
+            itemName.text = cardInfo.name;
+            StartCoroutine(LoadThumb());
+        }
+
+        private IEnumerator LoadThumb() 
+        {
+            yield return SpriteLoader.LoadSprite(cardInfo.cardURL, OnThumbLoaded);
+        }
+
+        private void OnThumbLoaded(Sprite sprite)
+        {
+            thumb.sprite = sprite;
+            thumb.preserveAspect = true;
+            cardInfo.imageSprite = sprite;
+        }
+
+        public void Show() 
+        {
+            if (isSelected) return;
+
+            shownObject.SetActive(true);
+            hiddenObject.SetActive(false);
+            isSelected = true;
+
+            onShow?.Invoke(cardInfo);
+        }
+
+        public void Hide() 
+        {
+            if (!isSelected) return;
+
+            shownObject.SetActive(false);
+            hiddenObject.SetActive(true);
+            isSelected = false;
         }
     }
 }
---
+++ Assets/Scripts/Utility/SpriteLoader.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace RickAndMemory.Utility
+{
+    public static class SpriteLoader
+    {
+        private static Dictionary<string, Sprite> spriteMap = new Dictionary<string, Sprite>();
+
+        public static IEnumerator LoadSprite(string url, Action<Sprite> callback) 
+        {
+            UnityWebRequest lRequest = UnityWebRequestTexture.GetTexture(url);
+            yield return lRequest.SendWebRequest();
+
+            if (lRequest.result == UnityWebRequest.Result.ConnectionError
+                || lRequest.result == UnityWebRequest.Result.ProtocolError
+                || lRequest.result == UnityWebRequest.Result.DataProcessingError)
+                Debug.Log("Error: " + lRequest.error);
+            else
+            {
+                Texture2D lTexture = new Texture2D(2, 2);
+                lTexture.LoadImage(lRequest.downloadHandler.data);
+                Sprite sprite = Sprite.Create(lTexture, new Rect(0.0f, 0.0f, lTexture.width, lTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
+                callback?.Invoke(sprite);
+            }
+        }
+    }
+}
