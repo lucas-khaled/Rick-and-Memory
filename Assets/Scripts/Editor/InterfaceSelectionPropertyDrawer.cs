d2c1879c16118c74112e554d4c32eba8bed829a9
lucas-khaled <khaledbrugger3@gmail.com>
2024-10-10 22:53:35 -0300

    Make GameSetup draw fields from each implementation

--- Assets/Scripts/Editor/InterfaceSelectionPropertyDrawer.cs
+++ Assets/Scripts/Editor/InterfaceSelectionPropertyDrawer.cs
@@ -1,6 +1,7 @@
 using RickAndMemory.Attributes;
 using System;
 using System.Linq;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,13 +10,22 @@ namespace RickAndMemory.Editor
     [CustomPropertyDrawer(typeof(InterfaceSelectionAttribute))]
     public class InterfaceSelectionPropertyDrawer : PropertyDrawer
     {
+        private bool folded;
+        private int propertyCounts;
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            EditorGUI.BeginProperty(position, label, property);
+
+            propertyCounts = 0;
+            var lineHeight = EditorGUIUtility.singleLineHeight;
+            var padding = EditorGUIUtility.standardVerticalSpacing;
+
             var attribute = (InterfaceSelectionAttribute)base.attribute;
 
             if (!attribute.InterfaceType.IsInterface)
             {
                 EditorGUI.LabelField(position, label.text, "Error: Not an interface!");
+                propertyCounts++;
                 return;
             }
 
@@ -31,16 +41,87 @@ namespace RickAndMemory.Editor
                 currentIndex = Array.IndexOf(options, property.managedReferenceValue.GetType().Name);
             }
 
+            position.height = lineHeight;
             int newIndex = EditorGUI.Popup(position, label.text, currentIndex, options);
+            position.y += lineHeight + padding;
+            propertyCounts++;
 
             if (newIndex >= 0 && newIndex < implementations.Length)
             {
                 var implementation = implementations[newIndex];
-                if (typeof(UnityEngine.Object).IsAssignableFrom(implementation))
-                    EditorGUI.ObjectField(position, property, implementation);
-                else
+                if(property.managedReferenceValue == null || property.managedReferenceValue.GetType() != implementation)
                     property.managedReferenceValue = Activator.CreateInstance(implementations[newIndex]);
+
+                DrawObjectProperties(new Rect(position), property.managedReferenceValue);
             }
+
+            EditorGUI.EndProperty();
+        }
+
+        private void DrawObjectProperties(Rect position, object obj)
+        {
+            var lineHeight = EditorGUIUtility.singleLineHeight;
+            var padding = EditorGUIUtility.standardVerticalSpacing;
+
+            EditorGUI.indentLevel++;
+            position.height = lineHeight;
+            folded = EditorGUI.BeginFoldoutHeaderGroup(position, folded, obj.GetType().Name);
+            
+            if (folded)
+            {
+                position.y += lineHeight + padding;
+                propertyCounts++;
+                FieldInfo[] fields = obj.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+                foreach (FieldInfo field in fields)
+                {
+                    object fieldValue = field.GetValue(obj);
+                    position.height = lineHeight;
+
+                    if (field.FieldType == typeof(int))
+                    {
+                        int newValue = EditorGUI.IntField(position, field.Name, (int)fieldValue);
+                        field.SetValue(obj, newValue);
+                    }
+                    else if (field.FieldType == typeof(float))
+                    {
+                        float newValue = EditorGUI.FloatField(position, field.Name, (float)fieldValue);
+                        field.SetValue(obj, newValue);
+                    }
+                    else if (field.FieldType == typeof(string))
+                    {
+                        string newValue = EditorGUI.TextField(position, field.Name, (string)fieldValue);
+                        field.SetValue(obj, newValue);
+                    }
+                    else if (field.FieldType == typeof(bool))
+                    {
+                        bool newValue = EditorGUI.Toggle(position, field.Name, (bool)fieldValue);
+                        field.SetValue(obj, newValue);
+                    }
+                    else if (typeof(UnityEngine.Object).IsAssignableFrom(field.FieldType))
+                    {
+                        UnityEngine.Object unityObj = EditorGUI.ObjectField(position, field.Name, (UnityEngine.Object)fieldValue, field.FieldType, false);
+                        field.SetValue(obj, unityObj);
+                    }
+                    else
+                        EditorGUI.LabelField(position, field.Name, "Unsupported type: " + field.FieldType.Name);
+
+                    position.y += lineHeight + padding;
+                    propertyCounts++;
+                }
+
+            }
+            
+            EditorGUI.EndFoldoutHeaderGroup();
+            EditorGUI.indentLevel--;
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var lineHeight = EditorGUIUtility.singleLineHeight;
+            var padding = EditorGUIUtility.standardVerticalSpacing;
+
+            return base.GetPropertyHeight(property, label) + (lineHeight*propertyCounts) + (padding * propertyCounts-1);
         }
     }
 }
