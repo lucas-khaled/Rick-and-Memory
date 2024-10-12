0c39b2aaa77f670e2421c8205cfa095a276ff9fa
lucas-khaled <khaledbrugger3@gmail.com>
2024-10-12 11:40:53 -0300

    make audio manager plays many audios at the same time

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,14 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace RickAndMemory.Audio
 {
-    [RequireComponent(typeof(AudioSource))]
     public class AudioManager : MonoBehaviour
     {
         public static AudioManager Instance { get; private set; }
-        private AudioSource source;
+
+        private List<AudioSource> playingSources = new List<AudioSource>();
+        private Queue<AudioSource> notPlayingSources = new Queue<AudioSource>();
 
         private void Awake()
         {
@@ -19,11 +21,13 @@ namespace RickAndMemory.Audio
             }
 
             Instance = this;
-            source = GetComponent<AudioSource>();
         }
 
         public void PlayClip(AudioClip clip, float pinch = 1, float delay = 0) 
         {
+            AudioSource source = GetSource();
+            playingSources.Add(source);
+
             source.clip = clip;
             source.pitch = pinch;
 
@@ -31,6 +35,32 @@ namespace RickAndMemory.Audio
                 source.PlayDelayed(delay);
             else
                 source.Play();
+
+            StartCoroutine(CheckForAudioFinished(source));
+        }
+
+        private AudioSource GetSource()
+        {
+            if(notPlayingSources.Count > 0)
+            {
+                return notPlayingSources.Dequeue();
+            }
+
+            GameObject sourceObject = new GameObject("source", typeof(AudioSource));
+            sourceObject.transform.SetParent(transform);
+
+            return sourceObject.GetComponent<AudioSource>();
+        }
+
+        private IEnumerator CheckForAudioFinished(AudioSource source) 
+        {
+            while (source.isPlaying) 
+            {
+                yield return null;
+            }
+
+            playingSources.Remove(source);
+            notPlayingSources.Enqueue(source);
         }
     }
 }
