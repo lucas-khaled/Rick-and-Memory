bb3f9a84c596fd1a26f7c68d20f8122896a4c54c
lucas-khaled <khaledbrugger3@gmail.com>
2024-10-12 22:24:44 -0300

    adds sprites to Dictionary in SpriteLoader

--- Assets/Scripts/Utility/SpriteLoader.cs
+++ Assets/Scripts/Utility/SpriteLoader.cs
@@ -25,8 +25,20 @@ namespace RickAndMemory.Utility
                 Texture2D lTexture = new Texture2D(2, 2);
                 lTexture.LoadImage(lRequest.downloadHandler.data);
                 Sprite sprite = Sprite.Create(lTexture, new Rect(0.0f, 0.0f, lTexture.width, lTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
+                AddToMap(url, sprite);
                 callback?.Invoke(sprite);
             }
+        }
+
+        private static void AddToMap(string url, Sprite sprite) 
+        {
+            if (spriteMap.ContainsKey(url) is false)
+            {
+                spriteMap.Add(url, sprite);
+                return;
+            }
+
+            spriteMap[url] = sprite;
         }
     }
 }
